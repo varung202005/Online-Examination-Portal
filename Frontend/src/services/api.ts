a71d40585799e4fb26ae7455dd8081680f991f44
Varun Gupta <varung202005@gmail.com>
2026-03-20 23:07:52 +0530

    deployment fix

--- Frontend/src/services/api.ts
+++ Frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://online-examination-portal-1-52p1.onrender.com",
   headers: {
     "Content-Type": "application/json"
   }
